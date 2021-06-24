735dd70171f32970ef010be6b68034e32467c383
bogdankol <bogdank208@gmail.com>
2021-06-24 12:01:05 +0300

    add interval stop + two-numbers time system

--- src/js/class.js
+++ src/js/class.js
@@ -5,23 +5,42 @@ class CountdownTimer {
   constructor({selector, targetDate}) {
     this.selector = selector;
     this.targetDate = targetDate.getTime();
+
   };
 
+  
+
   start() {
-    setInterval(() => {
+    this.id = setInterval(() => {
+
       const time = this.getTimeDifference();
+
+      this.stopData = this.targetDate - this.currentDate;
+
+      if (this.stopData <= 0) {
+
+        this.stop();
+        return;
+
+      } else {
+
+        this.otherTime = this.updateValues(time);
+        
+        }
+
       this.otherTime = this.updateValues(time);
+
     }, 1000)
   }
 
   getTimeDifference() {
 
-    const currentDate = new Date().getTime()
+    this.currentDate = new Date().getTime();
   
-    const daysLeft = getDays(this.targetDate - currentDate)
-    const hoursLeft = getHours(this.targetDate - currentDate)
-    const minutesLeft = getMins(this.targetDate - currentDate)
-    const secondsLeft = getSecs(this.targetDate - currentDate)
+    const daysLeft = getDays(this.targetDate - this.currentDate)
+    const hoursLeft = getHours(this.targetDate - this.currentDate);
+    const minutesLeft = getMins(this.targetDate - this.currentDate);
+    const secondsLeft = getSecs(this.targetDate - this.currentDate);
 
     return {daysLeft, hoursLeft, minutesLeft, secondsLeft}
   }
@@ -32,10 +51,20 @@ class CountdownTimer {
     refs.minutes.textContent = `${minutesLeft}`;
     refs.seconds.textContent = `${secondsLeft}`;
   }
+
+  stop() { 
+            
+    refs.days.textContent = '00';
+    refs.hours.textContent = '00';
+    refs.minutes.textContent = '00';
+    refs.seconds.textContent = '00';
+
+    this.id = 0;
+  }
+
 }
 
 module.exports = CountdownTimer;
-
 
 
 
--- src/js/methods.js
+++ src/js/methods.js
@@ -1,8 +1,12 @@
 
-const getDays = time => Math.floor(time / (1000 * 60 * 60 * 24));
-const getHours = time => Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const getMins = time => Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-const getSecs = time => Math.floor((time % (1000 * 60)) / 1000);
+const getDays = time => padTime(Math.floor(time / (1000 * 60 * 60 * 24)));
+const getHours = time => padTime(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+const getMins = time => padTime(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+const getSecs = time => padTime(Math.floor((time % (1000 * 60)) / 1000));
+
+function padTime(time) {
+    return String(time).padStart(2, `0`) // добавляем нолик если число имеет меньше одной цифры
+}
 
 const methods = { getDays, getHours, getMins, getSecs }
 
